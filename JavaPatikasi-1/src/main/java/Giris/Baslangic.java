0e2b288061aa52b22db4229f9e973e1fdd4704c7
ErenSahind <droppeed@gmail.com>
2022-10-16 20:05:25 +0300

    JavaPatikasi1-Added Variables

---
+++ JavaPatikasi-1/src/main/java/Giris/Baslangic.java
@@ -0,0 +1,45 @@
+package Giris;
+
+import java.util.Arrays;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        //tam sayılar
+        byte bit8 = -128; // -128 <= x < 128
+        short bit16 = 123; //16 bit
+        int bit32 = 14125; //32 bit
+        long bit64 = 125189275; //64 bit
+
+        //ondalık sayılar
+        float bit32F = 32.4F;
+        double bit64D = 123.123;
+
+        System.out.println("bit16 = " + bit16);
+
+
+        System.out.println("Hello World!");
+        System.out.print("Have we go! \"1\" " );
+        //System.out.println(5 > 13);
+        System.out.print("\n3\n2\n1");
+
+        System.out.println("Hello World!");
+        System.out.print("Have we go! \"1\" " );
+        //System.out.println(5 > 13);
+        System.out.print("\n3\n2\n1\n");
+
+                char c1='J';
+                char c2='A';
+                char c3='V';
+                char c4='A';
+        System.out.println(""+c1+c2+c3+c4); // ""=290 ascii
+        System.out.println("Bit Değeri = " + c1+c2+c3+c4); //string ifade olarak yazdırır
+        System.out.println(c1+c2+c3+c4); //ascii kodlarını toplar
+
+        //Java'da Stringler charlardan oluşan classlardır o yüzden büyük harfler başlar.
+
+
+
+
+    }
+
+}
